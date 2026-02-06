9a7397484037b96acc5178fecd0e8b715a550859
EgleSniuk <egle.sniukstaite@gmail.com>
2026-02-06 13:37:53 +0200

    mongoose and src.models.car.js

---
+++ src/models/car.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+
+const schema = mongoose.Schema({
+  id: { type: String, required: true },
+  brand: { type: String, required: true },
+  model: { type: String, required: true },
+  year: { type: Number, required: true },
+  imgUrl: { type: String, required: true },
+  userId: { type: String, required: true },
+});
+
+export default mongoose.model("Car", schema);
